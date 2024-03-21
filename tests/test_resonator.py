bb017ddd236470962853e0ed7d71eb614f53c230
Marco Gobbo <gobbo.marco98@gmail.com>
2024-03-21 17:04:46 +0100

    Update test resonator

--- tests/test_resonator.py
+++ tests/test_resonator.py
@@ -11,7 +11,7 @@ from two_coupled_qubits import Resonator
 @pytest.fixture(name="resonator")
 def fixture_resonator() -> Qobj:
     """Resonator object."""
-    return Resonator(n=5, w=7.00, c_r=368.440)
+    return Resonator(dim=5, w=7.00, c_r=368.440)
 
 
 def test_v_rms(resonator):
